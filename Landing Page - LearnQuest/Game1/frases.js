85305719a9fdb242f95446fb0604a05ffdf442d7
Gustavo Alves <103228297+alvesoff@users.noreply.github.com>
2024-11-29 15:52:22 -0300

    Add files via upload

---
+++ Landing Page - LearnQuest/Game1/frases.js
@@ -0,0 +1,9 @@
+const paragraphs = [
+    "Authors often misinterpret the lettuce as a folklore rabbi, when in actuality it feels more like an uncursed bacon. Pursued distances show us how mother-in-laws can be charleses.",
+    "Programming isn't just about writing code; it's about solving problems and creating something impactful."
+];
+  
+const translations = [
+    "Os autores frequentemente interpretam mal a alface como um rabino folclórico, quando na realidade parece mais um bacon não amaldiçoado. Distâncias perseguidas mostram-nos como as sogras podem ser charleses.",
+    "Programar não é apenas escrever código; é sobre resolver problemas e criar algo impactante."
+];
